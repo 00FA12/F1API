99a88e1e190222d25a706f24d9b5ce2be16d7298
00FA12 <114292738+00FA12@users.noreply.github.com>
2024-02-21 16:02:09 +0100

    two functions created, no test

--- GetPlotLaps.py
+++ GetPlotLaps.py
@@ -4,36 +4,37 @@ from matplotlib import pyplot as plt
 import fastf1
 import fastf1.plotting
 
-def getLaps():
+def getLaps(year, race, session, driver):
 
-    session = fastf1.get_session(2023, 'Monaco', 1)
+    session = fastf1.get_session(year, race, session)
     session.load()
 
-alo_laps = session.laps.pick_driver('ALO')
+    return session.laps.pick_driver(driver)
 
-fig, ax = plt.subplots(figsize=(8, 8))
+def createPlotGraph(dataSample):
+    fig, ax = plt.subplots(figsize=(8, 8))
 
-sns.scatterplot(data=alo_laps,
-                x="LapNumber",
-                y="LapTime",
-                ax=ax,
-                hue="Compound",
-                palette=fastf1.plotting.COMPOUND_COLORS,
-                s=80,
-                linewidth=0,
-                legend='auto')
+    sns.scatterplot(data=dataSample,
+                    x="LapNumber",
+                    y="LapTime",
+                    ax=ax,
+                    hue="Compound",
+                    palette=fastf1.plotting.COMPOUND_COLORS,
+                    s=80,
+                    linewidth=0,
+                    legend='auto')
 
-ax.set_xlabel("Lap Number")
-ax.set_ylabel("Lap Time")
+    ax.set_xlabel("Lap Number")
+    ax.set_ylabel("Lap Time")
 
-# The y-axis increases from bottom to top by default
-# Since we are plotting time, it makes sense to invert the axis
-ax.invert_yaxis()
-plt.suptitle("Alonso Laptimes in the 2024 Bahrain Testing S1x1")
+    # The y-axis increases from bottom to top by default
+    # Since we are plotting time, it makes sense to invert the axis
+    ax.invert_yaxis()
+    plt.suptitle("Alonso Laptimes in the 2024 Bahrain Testing S1x1")
 
-# Turn on major grid lines
-plt.grid(color='w', which='major', axis='both')
-sns.despine(left=True, bottom=True)
+    # Turn on major grid lines
+    plt.grid(color='w', which='major', axis='both')
+    sns.despine(left=True, bottom=True)
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
