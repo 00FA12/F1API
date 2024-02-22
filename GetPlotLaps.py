2123468899943ac924d2370b51b99e435edeec86
00FA12 <114292738+00FA12@users.noreply.github.com>
2024-02-22 08:29:28 +0100

    Errors with time axis and retrieving testing data

--- GetPlotLaps.py
+++ GetPlotLaps.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 from matplotlib import pyplot as plt
+import matplotlib.ticker as ticker
 
 import fastf1
 import fastf1.plotting
@@ -9,7 +10,13 @@ def getLaps(year, race, session, driver):
     session = fastf1.get_session(year, race, session)
     session.load()
 
-    return session.laps.pick_driver(driver)
+    return session.laps.pick_driver(driver).pick_quicklaps().reset_index()
+
+def format_time(x, pos=None):
+    minutes = int(x // 60)
+    seconds = int(x % 60)
+    milliseconds = int((x * 1000) % 1000)
+    return f'{minutes:02}:{seconds:02}:{milliseconds:03}'
 
 def createPlotGraph(dataSample):
     fig, ax = plt.subplots(figsize=(8, 8))
@@ -26,15 +33,21 @@ def createPlotGraph(dataSample):
 
     ax.set_xlabel("Lap Number")
     ax.set_ylabel("Lap Time")
+    ax.set_facecolor('xkcd:black')
 
     # The y-axis increases from bottom to top by default
     # Since we are plotting time, it makes sense to invert the axis
     ax.invert_yaxis()
-    plt.suptitle("Alonso Laptimes in the 2024 Bahrain Testing S1x1")
 
     # Turn on major grid lines
     plt.grid(color='w', which='major', axis='both')
     sns.despine(left=True, bottom=True)
+
+    # Create a formatter
+    formatter = ticker.FuncFormatter(format_time)
+
+    # Set the formatter
+    ax.yaxis.set_major_formatter(formatter)   
 
     plt.tight_layout()
     plt.show()
